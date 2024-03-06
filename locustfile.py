bd60b2d485430c0b6b80854845220f2df3b544ec
Thanh Luu <congthanhluu@gmail.com>
2024-03-06 10:18:00 +0000

    adding locustfile.py

---
+++ locustfile.py
@@ -0,0 +1,8 @@
+from locust import HttpUser, task
+
+
+class WebsiteUser(HttpUser):
+    @task
+    def predict(l):
+        l.client.post("/predict", { "CHAS":{"0":0},"RM":{"0":6.575},"TAX":{"0":296.0},"PTRATIO":{"0":15.3},"B":{"0":396.9},"LSTAT":{"0":4.98} })
+        l.client.get("/")
